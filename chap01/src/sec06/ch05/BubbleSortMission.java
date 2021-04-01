52fb0f203896454fc3c6a0cc3266001aa6ce59cc
ybo4117 <ybo4117@naver.com>
2021-04-01 16:25:14 +0900

    버블정렬 공부

---
+++ chap01/src/sec06/ch05/BubbleSortMission.java
@@ -0,0 +1,22 @@
+package sec06.ch05;
+
+import java.util.Arrays;
+
+public class BubbleSortMission {
+	public static void main(String[] args) {
+		int[] arr = { 8, 7, 3, 1, 6, 0 };
+		int temp = 0;		
+//		System.out.println(Arrays.toString(arr));
+		for (int i = 0; i < arr.length; i++) {			
+			for (int z = 0; z < arr.length-1; z++) {
+				if (arr[z] > arr[z+1]) {
+					temp = arr[z];
+					arr[z] = arr[z+1];					
+					arr[z+1] = temp;
+				}				
+				System.out.println(Arrays.toString(arr));
+			}
+			System.out.println();
+		}
+	}
+}
--- chap01/src/sec06/ch05/VendingMachineMisson3.java
+++ chap01/src/sec06/ch05/VendingMachineMisson3.java
@@ -9,7 +9,7 @@ public class VendingMachineMisson3 {
 		int[] menuPriceArr = { 1000, 1200, 1300, 1500, 2000, 2000 };
 
 		Scanner scan = new Scanner(System.in);
-		int sum = 0;
+
 		System.out.print("돈을 주입해 주세요 : ");
 		int money = scan.nextInt();
 		while (true) {
@@ -19,24 +19,18 @@ public class VendingMachineMisson3 {
 			}
 			System.out.print("선택 > ");
 			int select = scan.nextInt();
-			
+
 			if (select == 0) {
-				
-				System.out.printf("종료 - 사용한 금액은 : %,d원\n", sum);
 				System.out.printf("종료 - 남은 금액은 : %,d원\n", money);
 				break;
 			} else if (select >= 1 && select <= menuNmArr.length) {
-				if(money < menuPriceArr[select-1]) { 
+				if (money < menuPriceArr[select - 1]) {
 					System.out.printf("금액이 부족합니다. (남은 금액 %,d원)\n", money);
-					continue;					
-				}				
-				sum = sum + menuPriceArr[select-1];
-				money = money - menuPriceArr[select-1];
-				System.out.printf("%s를 선택하셨습니다.\n", menuNmArr[select - 1]);
-				System.out.printf("현재까지 사용한 금액은 : %,d원입니다.\n", sum);
-				System.out.printf("현재까지 남은 금액은 : %,d원입니다.\n", money);
-			}
-			else {
+					continue;
+				}
+				money -= menuPriceArr[select - 1];
+				System.out.printf("%s를 선택하셨습니다. 남은 금액은 : %,d원입니다 \n", menuNmArr[select - 1], money);
+			} else {
 				System.out.println("잘못선택하셨습니다.");
 			}
 			System.out.println();
